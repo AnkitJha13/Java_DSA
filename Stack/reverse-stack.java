b884a0f89f054c0da51790f3dba89c1e2b43d98f
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-10-25 21:19:41 +0530

    Create reverse-stack.java

---
+++ Stack/reverse-stack.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class StackA {
+
+    public static String reverseString(String str){
+        Stack<Character> s = new Stack<>();
+        int i = 0;
+        while(i < str.length()){
+            s.push(str.charAt(i));
+            i++;
+        }
+
+        StringBuilder result = new StringBuilder("");
+        while(!s.isEmpty()){
+            char curr = s.pop();
+            result.append(curr);
+        }
+
+        return result.toString();
+    }
+   
+    public static void main(String[] args) {
+        String str = "abc";
+        String result = reverseString(str);
+        System.out.println(result);
+    }
+}
+
+
+// input:-   abc
+// output:-  cba
