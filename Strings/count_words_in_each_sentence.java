712d2bf11924eccda3976af39b0a5ee92c14f2ab
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2025-02-26 00:19:23 +0530

    Create count_words_in_each_sentence.java

---
+++ Strings/count_words_in_each_sentence.java
@@ -0,0 +1,27 @@
+public class FindOccurrenceOfWords {
+    public static int wordOccurrence(String text, String targetWord) {
+        text = text.toLowerCase();     // Convert to lowercase for case-insensitive comparison
+        targetWord = targetWord.toLowerCase();
+
+        String[] words = text.split("\\s+");  // Splitting sentence into words
+
+        int count = 0;
+        for (String word : words) {
+            if (word.equals(targetWord)) {   // Check if word matches target word
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String text = "Java Game java Mortal";
+        String targetWord = "java";
+
+        int occurrences = wordOccurrence(text, targetWord);
+        System.out.println("No of occurrences of '" + targetWord + "' is --> " + occurrences);
+    }
+}
+
+// Output 
+No of occurrences of 'a' is --> 0
